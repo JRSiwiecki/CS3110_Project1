b99e9bd4e6d165855a3cab19df1d37a7b6c9acbc
JRSiwiecki <JRSiwiecki@gmail.com>
2022-04-12 20:15:00 -0700

    Initial commit.

---
+++ StringToFloatDFA.java
@@ -0,0 +1,14 @@
+public class StringToFloatDFA 
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static float stringToFloat(String input)
+    {
+
+
+        return 0f;
+    }
+}
